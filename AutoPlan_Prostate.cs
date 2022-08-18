5332f7559dba675ef0e5bc51bc41f6e72ef2d159
Hudson Ukass <hudson.ukass@outlook.com>
2022-08-18 11:09:22 -0700

    Added course calculation;

--- AutoPlan_Prostate.cs
+++ AutoPlan_Prostate.cs
@@ -18,6 +18,8 @@ namespace AutoPlan_Prostate
 {
     class Program
     {
+        private static string _patientId;
+
         [STAThread]
         static void Main(string[] args)
         {
@@ -32,10 +34,54 @@ namespace AutoPlan_Prostate
             {
                 Console.Error.WriteLine(e.ToString());
             }
+            Console.ReadLine();
         }
         static void Execute(Application app)
         {
-            // TODO: Add your code here.
+            if(string.IsNullOrEmpty(_patientId))
+            {
+                Console.WriteLine("Enter Patient Id:");
+                _patientId = Console.ReadLine();
+            }
+
+            Patient patient = app.OpenPatientById(_patientId);
+            if (patient == null)
+            {
+                Console.WriteLine($"Could not find {_patientId}"); 
+                return;
+            }
+            Console.WriteLine($"Patient: {patient.Name} opened.");
+            
+            //must have this line for write-enabled scripting.
+            patient.BeginModifications();
+            //create a course.
+            Course course = null;
+            if (patient.Courses.Any(c => c.Id.Equals("AutoCourse")))
+            {
+                course = patient.Courses.FirstOrDefault(c => c.Id.Equals("AutoCourse"));
+            }
+            else { course = patient.AddCourse(); course.Id = "AutoCourse"; }
+            //Structure set Ids are not unique, so a check for the Image Id is needed as well.
+            ExternalPlanSetup plan = course.AddExternalPlanSetup(patient.StructureSets.FirstOrDefault(ss=> ss.Id.Equals("CT_1") && ss.Image.Id.Equals("CT_2")));
+            Console.WriteLine($"Plan: {plan.Id} in course {course.Id}.");
+            double[] gantryAngles = new double[] { 220, 255, 290, 325, 25, 55, 95, 130 };
+            ExternalBeamMachineParameters parameters = new ExternalBeamMachineParameters("HESNS", "10X", 600, "STATIC",null);
+            foreach (double ga in gantryAngles)
+            {
+                plan.AddStaticBeam(parameters,
+                    new VRect<double>(-50,-50,50,50),
+                    0,
+                    ga,
+                    0,
+                    plan.StructureSet.Image.UserOrigin);
+            }
+            Console.WriteLine($"Generated {plan.Beams.Count()} fields");
+            plan.SetPrescription(28, new DoseValue(250,DoseValue.DoseUnit.cGy), 1.0); //set rx.
+            Console.WriteLine("Calculating Dose...");
+            plan.CalculateDose();
+            Console.WriteLine("Saving...");
+            app.SaveModifications();
+
         }
     }
 }
